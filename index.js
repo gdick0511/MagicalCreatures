02f58f26bb0dd009f44e32fd1ae70909ae3182ca
garrettdick <garrettd0511@gmail.com>
2021-10-05 12:52:18 -0500

    added submit function so that the database is updated with the new creature

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log('Hello World')
 
 // document.addEventListener('DOMContentLoaded', renderCreatures)
 
+document.getElementById('form').addEventListener('submit', handleSubmit)
+
 function renderCreatures(creature){
     let card = document.createElement('li')
     card.className = 'card'
@@ -26,7 +28,33 @@ function getAllCreatures(){
             
             // .then(creatureData => creatureData.forEach(creature => renderCreatures(creature)))
 }
-        
+
+
+
+function handleSubmit(e){
+    e.preventDefault()
+    let creatureObj = {
+        name:e.target['name'].value,
+        imageUrl:e.target['picture'].value,
+        description:e.target['description'].value,
+        donations: 0
+    }
+    renderCreatures(creatureObj)
+    addCreature(creatureObj)
+}
+
+function addCreature(creatureObj){
+    fetch('http://localhost:3000/creatureData',{
+        method: "POST",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(creatureObj)
+    })
+    .then(res => res.json())
+    .then(creature => console.log(creature))
+}
+
 function initialize(){
  getAllCreatures()
 }
