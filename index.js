2f6080a7997c0e1a99fe78b7b93130105d322fa3
garrettdick <garrettd0511@gmail.com>
2021-10-05 10:25:24 -0500

    reformatted the HTML

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 console.log('Hello World')
 
+function renderCreatures(creatures){
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+        <img src='${creatures.imageUrl}'>
+        <div class='content'>
+            <h4>${creatures.name}
+            <p>
+                $<span class='donation_count">${creatures.donations}</span> Donated
+            </p>
+            <p>${creatures.description}</P>
+        </div>    
+    `
+    document.querySelector('#creature_list').appendChild(card)
+}
